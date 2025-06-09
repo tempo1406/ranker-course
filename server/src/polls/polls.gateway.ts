f70775010f8627eb0dfec4649a68038070bb56ef
gakfc1406 <gakfc1406@gmail.com>
2025-06-09 16:25:10 +0700

    setup socket.io

---
+++ server/src/polls/polls.gateway.ts
@@ -0,0 +1,14 @@
+import { Logger } from "@nestjs/common";
+import { OnGatewayInit, WebSocketGateway } from "@nestjs/websockets";
+import { PollsService } from "./polls.service";
+
+@WebSocketGateway({
+    namespace: "polls",
+})
+export class PollsGateway implements OnGatewayInit {
+    private readonly logger = new Logger(PollsGateway.name);
+    constructor(private readonly pollsService: PollsService) {}
+    afterInit(server: any) {
+        this.logger.log('WebSocket server initialized for polls namespace');
+    }
+}
